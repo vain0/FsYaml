1f8d9ba0d793a85722cfa2bddd6f2a65c39708fd
bleis-tift <bleis-tift@hotmail.co.jp>
2012-04-26 22:42:06 +0900

    型クラス採用して書き直す？ refs #12
    参考：
    * http://www.nut-cracker.com.ar/
    * http://code.google.com/p/fsharp-typeclasses/

---
+++ FsYaml.Tests/YamlSpec.fs
@@ -0,0 +1,36 @@
+﻿module YamlSpec
+
+open NUnit.Framework
+open NaturalSpec
+
+module 整数 =
+  [<Example("42", 42)>]
+  [<Example("0", 0)>]
+  let ``数値のみを含むYAML文字列を数値に変換できる``(yaml, expected) =
+    Given yaml
+    |> When (fun yaml -> let r: int = Yaml.load yaml in r)
+    |> It should equal expected
+    |> Verify
+
+module 文字列 =
+  [<Example("42")>]
+  [<Example("hoge")>]
+  let ``文字列のみを含むYAML文字列を文字列に変換できる``(yaml) =
+    Given yaml
+    |> When (fun yaml -> let r: string = Yaml.load yaml in r)
+    |> It should equal yaml
+    |> Verify
+
+module 独自の型 =
+  type t = Hoge | Piyo with
+    static member (?<-) (yaml: string, _Format: Yaml.Read, _: t) =
+      if yaml.ToLower() = "hoge" then Hoge else Piyo
+
+  [<Example("HoGE", true)>]
+  [<Example("piyo", false)>]
+  [<Example("foo", false)>]
+  let 独自の型に独自の変換を定義できる (yaml, isHoge) =
+    Given yaml
+    |> When (fun yaml -> let r: t = Yaml.load yaml in r)
+    |> It should equal (if isHoge then Hoge else Piyo)
+    |> Verify
---
+++ FsYaml/Yaml.fs
@@ -0,0 +1,17 @@
+﻿module Yaml
+
+// Format class ---------------------------------------------------------------
+
+type Read = Read with
+  static member (?<-) (yaml: string, _Format: Read, _: int) = int yaml
+  static member (?<-) (yaml: string, _Format: Read, _: string) = yaml
+
+type Write = Write with
+  static member (?<-) (_, _Format: Write, i: int) = string i
+  static member (?<-) (_, _Format: Write, s: string) = s
+
+let inline load (yaml: string) : ^a =
+  yaml ? (Read) <- Unchecked.defaultof< ^a >
+
+let inline dump x : string =
+  () ? (Write) <- x
