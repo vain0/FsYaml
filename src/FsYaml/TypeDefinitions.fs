11bb0c17061cf4814607497f1933128c49cdf42d
vain0 <id_vain@yahoo.co.jp>
2016-05-12 13:00:16 +0900

    Format

--- src/FsYaml/TypeDefinitions.fs
+++ src/FsYaml/TypeDefinitions.fs
@@ -7,8 +7,17 @@ open FsYaml.RepresentationTypes
 open FsYaml.NativeTypes
 open FsYaml.CustomTypeDefinition
 
-let intDef = { Accept = (=)typeof<int>; Construct = constructFromScalar int; Represent = representAsPlain string }
-let int64Def = { Accept = (=)typeof<int64>; Construct = constructFromScalar int64; Represent = representAsPlain string }
+let intDef = {
+  Accept = (=)typeof<int>
+  Construct = constructFromScalar int
+  Represent = representAsPlain string
+}
+let int64Def = {
+  Accept = (=)typeof<int64>
+  Construct = constructFromScalar int64
+  Represent = representAsPlain string
+}
+
 let floatDef = {
   Accept = (=)typeof<float>
   Construct = constructFromScalar (String.toLower >> function
@@ -25,7 +34,11 @@ let floatDef = {
       else string n
     Scalar (Plain text, None)
 }
-let stringDef = { Accept = (=)typeof<string>; Construct = MaybeNull.constructFromScalar id; Represent = MaybeNull.representAsNonPlain string }
+let stringDef = {
+  Accept = (=)typeof<string>
+  Construct = MaybeNull.constructFromScalar id
+  Represent = MaybeNull.representAsNonPlain string
+}
 let boolDef = {
   Accept = (=)typeof<bool>
   Construct = constructFromScalar (String.toLower >> function
@@ -36,7 +49,11 @@ let boolDef = {
     let text = unbox<bool> obj |> string |> String.toLower 
     Scalar (Plain text, None)
 }
-let decimalDef = { Accept = (=)typeof<decimal>; Construct = constructFromScalar decimal; Represent = representAsPlain string }
+let decimalDef = {
+  Accept = (=)typeof<decimal>
+  Construct = constructFromScalar decimal
+  Represent = representAsPlain string
+}
 let datetimeDef = {
   Accept = (=)typeof<DateTime>
   Construct = constructFromScalar (fun x -> DateTime.Parse(x))
